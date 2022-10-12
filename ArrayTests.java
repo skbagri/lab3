de48900449c9bbf8a7bfe1e91e37b62f1acf63c8
skbagri <sbagri@ucsd.edu>
2022-10-12 14:36:56 -0700

    ArrayExamples fix

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlaceBug() {
+    int[] input1 = {3, 5, 7};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{7, 5, 3}, input1);
+	}
+
+  @Test
+  public void testReversedBug() {
+    int[] input1 = {2, 4, 6};
+    assertArrayEquals(new int[]{6, 4, 2}, ArrayExamples.reversed(input1));
   }
 }
